101c9af18f96026e0271b27028eebd6ff74bad08
ubaidullah0511 <ubaid05011@gmail.com>
2024-06-02 14:06:25 +0500

    logo added

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -7,9 +7,9 @@ const navbar = () => {
     return (
 
         <div className="main  w-screen h-20 flex items-start justify-evenly sticky top-0 bg-gradient-to-t from-30% from-slate-900 to-slate-800 z-10">
-            <div className="logo w-1/3 h-full flex items-center">
-                <img className='h-full filter' src={logo} alt="" />
-                <h1 className="text-3xl text-white h-full">Career Funda</h1>
+            <div className="logo w-1/3 h-full flex items-center gap-5">
+                <img className='h-2/3 filter' src={logo} alt="" />
+                <h1 className="text-3xl text-white h-2/3">Career Funda</h1>
             </div>
             <ul className="navcontent w-1/3 h-3/4 flex justify-between items-start list-none">
                 <li className='h-full'><Link to="/"><Navcomp compname="Home" /></Link></li>
